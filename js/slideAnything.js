689f25144832680de9126266810dddac49c6f731
Jonathan <jon.vargas.c@gmail.com>
2013-05-28 17:31:43 -0400

    set up for the jquery plugin
    what the title says

---
+++ js/slideAnything.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($){
+    $.fn.slideAnything = function(options {  
+ 
+        var opt = {
+            // Options
+        };
+ 
+        return "hola";//this.each(function() {        
+            /* this is where the magic starts */
+            /*if (options) { 
+                $.extend(opt, options);
+            }*/
+                       
+        });
+    };
+});
