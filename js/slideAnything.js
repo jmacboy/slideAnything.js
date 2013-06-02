d87f2380721b5a4cf9bb6aac0c6bb83cb0379837
Jonathan <jon.vargas.c@gmail.com>
2013-06-02 15:36:01 -0400

    improvement on html generation
    > ok so i realized i didn't need the tag Name to cycle thru the slider
    since .children() abstracts it so well.
    > created the function 'AssignDisplay' with comments. cleaning up the
    code and making it more generic.

--- js/slideAnything.js
+++ js/slideAnything.js
@@ -18,31 +18,11 @@ jQuery(document).ready(function () {
                 'margin': 0,
                 'padding': 0
             });
-        var sliderType = slider.prop('tagName');
-        /*
-            we detect which type of slider we're building so as to be able to know
-            if we'll search through a list of <li> tags or <div> tags
-        */
-        if(sliderType === 'UL'){
-        	slider.find('li').each(function(){
-            if(sliderLength != 0){
-        		$(this).children(':first').addClass('hiddenElement');
-        	}
-        	sliderLength++;
-        	});
-        }else {
-        	slider.find('div').each(function(){
-        	if(sliderLength != 0){
-        		$(this).children(':first').addClass('hiddenElement');
-        	}
-        	sliderLength++;
-        	});
-        }
-        
+        sliderLength = AssignDisplay(0, slider);
         /* Container
 			-----------------------------------------------*/
 		// we create the div parent of the slider that'll contain our navigation arrows and points, as well as points.
-        var sliderParent = '<div class="slideAnything" style="height: ' + options['height'] + 'px; width: ' + options['width'] + 'px;" />';
+        var sliderParent = '<div class="slideAnything" style="height: ' + options['height'] + 'px; width: ' + options['width'] + 'px; margin: 0px auto 0px;" />';
         slider.wrap(sliderParent);//wraps the element that called the function and inserts it into the parent of our slider
         sliderParent = slider.parent();// we re-use the parent variable and make it equal to the DOM parent that we created.
         /* Previous and Next Buttons
@@ -56,12 +36,41 @@ jQuery(document).ready(function () {
         //and to finally end the html setup we add the nav points that are created based on the number of objects the slider contains
         var navPoints = '<div class="contenedornavegacion">'+
         					'<ul class="navegacion">';
-        for (var i = 0;  i <= sliderLength; i++) {
+        for (var i = 0;  i < sliderLength; i++) {
         		var idx = i+1;
        			navPoints+= '<li><a href="#">'+idx+'</a></li>';
        	}
        	navPoints += '</ul></div>';
        	sliderParent.append(navPoints);
         return slider;
+    }
+
+    /*
+        function that's primarily used for assigning the class
+        > Parameters:
+        - index: which index will be the 'current' element which will be visible.
+        - element: the DOM element which was user defined to be a slider.
+    */
+    function AssignDisplay(index, element){
+        var i = 0;
+        /*var sliderType = element.prop('tagName');
+        var tagType = (sliderType === 'UL')? 'li': 'div';*/
+        element.children().each(function(){
+            if( i === index){
+                $(this).css({
+                    display: 'block'
+                });
+            }else{
+                $(this).css({
+                    display: 'none'
+                });
+            }
+            i++;
+        });
+        /*
+            in an early attempt at multi-tasking the function it returns how many children
+            the slider has to know how many navPoints there needs to be.
+        */
+        return i;
     }
 });
