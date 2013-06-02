7a9e9e232b4c2cbd507058145c95a0b6d02dd083
Jonathan <jon.vargas.c@gmail.com>
2013-06-02 05:08:50 -0400

    css fixes, html generation
    * fixed some inefficient properties in css
    * html for the slider is now generated in jquery

--- js/slideAnything.js
+++ js/slideAnything.js
@@ -1,67 +1,48 @@
-jQuery(document).ready(function($){
-    $.fn.slideAnything = function(options {  
- 
-        var opt = {
-			'width': 908, // Width and height of the images
-			'height': 340,
-			'position': 'bottom', // Posicion
-			'bullets': false, // Show numbering navigation
-			'thumbs':  true, // Show thumbnail navigation
-			'row': 10, // Thumbnails per row
-			'auto': true, // Auto rotate
-			'autoSpeed': 4000,
-			'fadeSpeed': 1000
-		};
+jQuery(document).ready(function () {
+    $.fn.slideit = function (options) { 
+ 		/*
+        	Initial values that the slider will have by default. 
+        	user CAN put a custom width and height.
+        */
+        var defaults = {
+            width: 620,
+            height: 310
+        };
 
-		this.each(function() {        
-		
-			if (options) { 
-				$.extend(opt, options);
-			}
-
-			/* Contenedor
-			-----------------------------------------------*/
-			$(this).children().wrapAll('<div class="slider" />');
-			var contenedor = $(this).find('.slider');
-			contenedor.find('img').wrapAll('<div class="wrapp" />');
-			var wrapp = contenedor.find('.wrapp');
-
-            /* Anterior & Siguiente Botones
-			-----------------------------------------------*/
-			wrapp.append('<a href="#" class="prev">Anterio</a><a href="#" class="next">Siguiente</a>');
-
-
-			/* Navegacion 
-			-----------------------------------------------*/
-			switch (opt.position) { // Posicion
-					case 'top': container.prepend('<div class="navegacion" />'); break;
-					case 'bottom': container.append('<div class="navegacion" />'); break;
-			}
-
-			var nav = contenedor.find('.navegacion');
-
-			wrapp.find('img').each(function(i){
-
-				i += 1; // Inicia en 1
-
-				if (opt.bullets === true) { // Bullet navigation
-						navegacion.append('<a href="#">'+ i +'</a>'); 
-				}
-
-				if (opt.thumbs === true) { // Thumbnail navigation
-						nav.addClass('thumbs').append(
-							'<img class="thumb" src="'+
-							$(this).attr('src') +'" alt=""/>'); 
-				}
-
-				
-			});
-        return "hola";//this.each(function() {        
-            /* this is where the magic starts */
-            /*if (options) { 
-                $.extend(opt, options);
-            }*/
-                       
+		options = $.extend(defaults, options);
+        var sliderLength = 0;
+        var slider = $(this);
+        slider.find('div').each(function(){
+        	/*var w = $(this).children(':first').attr('src');
+        	console.log(w+" i: "+sliderLength);*/
+        	if(sliderLength != 0){
+        		$(this).children(':first').addClass('hiddenElement');
+        	}
+        	sliderLength++;
         });
-    };
+        /* Container
+			-----------------------------------------------*/
+		// we create the div parent of the slider that'll contain our navigation arrows and points, as well as points.
+        var sliderParent = '<div class="slideAnything" style="height: ' + options['height'] + 'px; width: ' + options['width'] + 'px;" />';
+        slider.wrapAll(sliderParent);//wraps the element that called the function and inserts it into the parent of our slider
+        sliderParent = slider.parent();// we re-use the parent variable and make it equal to the DOM parent that we created.
+        /* Previous and Next Buttons
+			-----------------------------------------------*/
+        sliderParent.append('<div class="botonesnav">'+
+        						'<div style="line-height: 17px;" ><a href="#" style="left: 0;">Prv</a></div>'+
+        						'<div style="line-height: 17px;" ><a href="#" style="right: 0;">Nxt</a></div>'+
+        						'</div>');//adding the previous and next buttons
+        /* Nav Points 
+			-----------------------------------------------*/
+        //and to finally end the html setup we add the nav points that are created based on the number of objects the slider contains
+        var navPoints = '<div class="contenedornavegacion">'+
+        					'<ul class="navegacion">';
+        for (var i = 0;  i <= sliderLength; i++) {
+        		var idx = i+1;
+       			navPoints+= '<li><a href="#">'+idx+'</a></li>';
+       	}
+       	navPoints += '</ul></div>';
+       	sliderParent.append(navPoints);
+        return slider;
+    }
 });
