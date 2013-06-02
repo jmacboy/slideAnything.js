2645418e96891e10a67fb84e8e872e6e1d42b7bd
Jonathan <jon.vargas.c@gmail.com>
2013-06-02 14:45:24 -0400

    finished html generation
    html generation is complete and it can be used for both <ul> and <div>
    type lists.

--- js/slideAnything.js
+++ js/slideAnything.js
@@ -12,19 +12,38 @@ jQuery(document).ready(function () {
 		options = $.extend(defaults, options);
         var sliderLength = 0;
         var slider = $(this);
-        slider.find('div').each(function(){
-        	/*var w = $(this).children(':first').attr('src');
-        	console.log(w+" i: "+sliderLength);*/
+        /* list of css specifically set due to the possible use of a UL element. */
+        slider.css({
+                'list-style': 'none',
+                'margin': 0,
+                'padding': 0
+            });
+        var sliderType = slider.prop('tagName');
+        /*
+            we detect which type of slider we're building so as to be able to know
+            if we'll search through a list of <li> tags or <div> tags
+        */
+        if(sliderType === 'UL'){
+        	slider.find('li').each(function(){
+            if(sliderLength != 0){
+        		$(this).children(':first').addClass('hiddenElement');
+        	}
+        	sliderLength++;
+        	});
+        }else {
+        	slider.find('div').each(function(){
         	if(sliderLength != 0){
         		$(this).children(':first').addClass('hiddenElement');
         	}
         	sliderLength++;
-        });
+        	});
+        }
+        
         /* Container
 			-----------------------------------------------*/
 		// we create the div parent of the slider that'll contain our navigation arrows and points, as well as points.
         var sliderParent = '<div class="slideAnything" style="height: ' + options['height'] + 'px; width: ' + options['width'] + 'px;" />';
-        slider.wrapAll(sliderParent);//wraps the element that called the function and inserts it into the parent of our slider
+        slider.wrap(sliderParent);//wraps the element that called the function and inserts it into the parent of our slider
         sliderParent = slider.parent();// we re-use the parent variable and make it equal to the DOM parent that we created.
         /* Previous and Next Buttons
 			-----------------------------------------------*/
